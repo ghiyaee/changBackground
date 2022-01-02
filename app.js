6fe72788c0abf870410fdb589c01fdba6d0d6f81
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-01-02 23:44:53 +0330

    update selectors

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const colors = ['lightblue', 'lightsalmon', 'lightyellow', 'lightgreen', 'lightgrey', 'crimson', 'pink'];
-const news = document.querySelectorAll('.colors');
 const body = document.querySelector('body'); 
 const stopRandom=  setInterval(chang,5000)
 document.body.onload = function(){
@@ -9,19 +8,18 @@ function chang() {
          ranDomShow();     
 }
 function ranDomShow() {
-    const rand = Math.floor(Math.random() * colors.length);
-    console.log(rand,colors);
+       const rand = Math.floor(Math.random() * colors.length);
        document.querySelector('body').style.backgroundColor = colors[rand];
        document.querySelector('#name-color').innerHTML = colors[rand];
        const color = document.querySelectorAll('.colors');
        color[rand+1].classList.add('active');
-   color[rand+1].innerHTML++;
+       color[rand+1].innerHTML++;
    
   }
-colors.forEach(p => {
+colors.forEach(color => {
     const btn = document.createElement('button');
     document.querySelector('.colors').appendChild(btn);
-    btn.style.backgroundColor = p;
+    btn.style.backgroundColor = color;
     btn.classList.add('colors');
 });
     
