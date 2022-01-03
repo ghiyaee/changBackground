8e3216d3d9f0058facf6204badcfbd18c326907c
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-01-03 15:31:29 +0330

    update and modify

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const colors = ['lightblue', 'lightsalmon', 'lightyellow', 'lightgreen', 'lightgrey', 'crimson', 'pink'];
-const body = document.querySelector('body'); 
+const div = document.querySelector('div');
+const body = document.querySelector('body');
 const stopRandom=  setInterval(chang,5000)
 document.body.onload = function(){
     ranDomShow();
@@ -12,15 +13,20 @@ function ranDomShow() {
        document.querySelector('body').style.backgroundColor = colors[rand];
        document.querySelector('#name-color').innerHTML = colors[rand];
        const color = document.querySelectorAll('.colors');
-       color[rand+1].classList.add('active');
-       color[rand+1].innerHTML++;
+       color[rand].classList.add('active');
+       color[rand].innerHTML++;
+       color[rand].style.transform=' rotateY(360deg)';
    
   }
 colors.forEach(color => {
-    const btn = document.createElement('button');
-    document.querySelector('.colors').appendChild(btn);
-    btn.style.backgroundColor = color;
-    btn.classList.add('colors');
+  const btn = document.createElement('button');
+  const conter = document.createElement('p');
+  conter.classList.add('conter')
+  div.appendChild(btn);
+  div.appendChild(conter);
+  btn.style.backgroundColor = color;
+  btn.classList.add('colors');
+  conter.innerHTML="conter"
 });
     
     
